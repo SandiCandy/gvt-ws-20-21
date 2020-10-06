c4d58680972839b4434b5be011c5c9e2d1ed24ca
Sandra Tieben <sandra@Sandras-MacBook-Pro.local>
2020-10-06 15:50:24 +0200

    ESA1 started

--- ESA1/script.js
+++ ESA1/script.js
@@ -4,18 +4,47 @@
 
 const greeting = "moin!"
 
-function loadImage(filename) {
-    alert("Load...")
-    var imageObj = new Image();
-    imageObj.src = "file:///Users/sandra/Desktop/GVT/ESA1/images/" + filename;
-    imageObj.onload = function() {
-        var img = document.getElementById('x');
-        img.setAttribute('src', this.src);
-    };
-    
+window.onkeydown = function(evt) {
+	var key = evt.which ? evt.which : evt.keyCode;
+
+	var c = String.fromCharCode(key);
+
+	switch(c){
+		case('R'):
+            rotateRight();
+		break;
+		case('L'):
+            rotateLeft();
+		break;
+		case('S'):
+            // triggerDiscAnimation();
+            console.log("Spin")
+            break;
+        default:
+            console.log('Miaaaau')
+	}
 }
 
-function myFunc(text) {
-    alert(text);
+function rotateRight() {
+    let view = document.getElementById('view');
+    let intNewPosY = parseInt(view.style.backgroundPositionY) - 758;
+    view.style.backgroundPositionY = intNewPosY + "px";
 }
 
+function rotateLeft() {
+    let view = document.getElementById('view');
+    let intNewPosY = parseInt(view.style.backgroundPositionY) + 758;
+    view.style.backgroundPositionY = intNewPosY + "px";
+}
+
+function loadSprite(filename) {
+    console.log('looooooooad.')
+    let imgObj = new Image();
+    imgObj.src = "file:///Users/sandra/Desktop/GVT/ESA1/images/" + filename;
+    imgObj.onload = function() {
+        let view = document.getElementById('view');
+        view.style.backgroundImage = "url('file:///Users/sandra/Desktop/GVT/ESA1/images/css_sprites.png')";
+        view.style.backgroundPosition = "-10px -10px";
+        view.style.backgroundRepeat = "no-repeat"
+    }
+}
